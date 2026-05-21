dbbb06f67b2a592871ec0c5b7f2df7a0c36b7843
Harshraj Singh Tomar <tomarharshrajsingh2@gmail.com>
2026-05-21 18:05:17 +0530

    list comprehension

--- 14_3darray.py
+++ 14_3darray.py
@@ -1,2 +1,11 @@
 rooms=[[[False for r in range(20)]for i in range(15)]for t in range(3)]
 print(rooms)
+
+rooms[1][9][13]=True
+rooms[1][9][1]=True
+
+vacancy=0
+for room_number in range(20):
+    if not rooms[1][9][room_number]:
+        vacancy+=1
+print(f"Number of vacant rooms: {vacancy}")
