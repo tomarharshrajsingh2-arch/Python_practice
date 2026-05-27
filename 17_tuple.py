738efcaf4f2e5beb6fa43d7820c609db8961132d
Harshraj Singh Tomar <tomarharshrajsingh2@gmail.com>
2026-05-27 16:55:49 +0530

    tuple

---
+++ 17_tuple.py
@@ -0,0 +1,53 @@
+# tuple=(1,10,100)
+# print(tuple)
+# print(type(tuple))
+# print(len(tuple))
+# tuple_1=tuple+(1000,10000)
+# print(tuple_1)
+# print(len(tuple_1))
+# tuple_2=tuple*3
+# print(tuple_2)
+# print(len(tuple_2))
+# tuple_3=(3,2,1)
+# tuple_3=(1,2)
+# print(tuple_3)
+
+# tuple=(1,10,100)
+# for elem in tuple:
+#     print(elem)
+
+# print(10 in tuple)
+# print(10 not in tuple)
+
+# tuple_1=(1000,10000)
+# tuple_2=tuple+tuple_1   
+# print(tuple_2)
+# tuple_3=tuple*3
+# print(tuple_3)
+
+# my_tuple = (1, 2, 3, 4, 5,"str")
+# print(my_tuple)  
+# print(type(my_tuple))
+
+# my_list=[1,2,3]
+# print(my_list)
+# print(type(my_list))
+# tup=tuple(my_list)
+# print(tup)  
+# print(type(tup))
+
+# var=123
+# t1=(1,)
+# t2=(2,)
+# t3=(3,var)
+# print(t1)
+# print(t2)   
+# print(t3)
+
+# t1,t2,t3=t3,t1,t2
+# print(t1)   
+# print(t2)
+# print(t3)
+
+# print(type(t1), type(t2), type(t3))
+
