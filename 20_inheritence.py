4291c1b106b64ada3c6e4dc99ae120cb9f6c3aa7
Harshraj Singh Tomar <tomarharshrajsingh2@gmail.com>
2026-06-08 17:56:06 +0530

    some more oops concept

--- 20_inheritence.py
+++ 20_inheritence.py
@@ -31,32 +31,67 @@
 # print(obj.supvar)
 
 
-class Level1:
-    variable_1 = 100
-    def __init__(self):
-        self.var_1 = 101
-    def fun_1(self):
-        return 102
+# class Level1:
+#     variable_1 = 100
+#     def __init__(self):
+#         self.var_1 = 101
+#     def fun_1(self):
+#         return 102
 
-class Level2(Level1):
-    variable_2=200
-    def __init__(self):
-        super().__init__()
-        self.var_2=201
-    def fun_2(self):
-        return 202
+# class Level2(Level1):
+#     variable_2=200
+#     def __init__(self):
+#         super().__init__()
+#         self.var_2=201
+#     def fun_2(self):
+#         return 202
 
-class Level3(Level2):
-    variable_3=300
-    def __init__(self):
-        super().__init__()
-        self.var_3=301
-    def fun_3(self):
-        return 302
+# class Level3(Level2):
+#     variable_3=300
+#     def __init__(self):
+#         super().__init__()
+#         self.var_3=301
+#     def fun_3(self):
+#         return 302
     
-obj=Level3()
-print(obj.variable_1,obj.var_1,obj.fun_1())
-print(obj.variable_2,obj.var_2,obj.fun_2())
-print(obj.variable_3,obj.var_3,obj.fun_3())
+# obj=Level3()
+# print(obj.variable_1,obj.var_1,obj.fun_1())
+# print(obj.variable_2,obj.var_2,obj.fun_2())
+# print(obj.variable_3,obj.var_3,obj.fun_3())
 
- 
+# print(isinstance(obj,Level1))
+
+# class Vehicle:
+#     pass
+# class LandVehicle(Vehicle):
+#     pass
+# class TrackedVehicle(LandVehicle):
+#     pass
+# my_vehicle =Vehicle()
+# My_land_vehicle=LandVehicle()
+# My_tracked_vehicle=TrackedVehicle()
+# for obj in[my_vehicle,My_land_vehicle,My_tracked_vehicle]:
+#     for cls in[Vehicle,LandVehicle,TrackedVehicle]:
+#         print(isinstance(obj,cls),end="\t")
+#     print()
+
+class SampleClass:
+    def __init__(self,val):
+        self.val = val
+
+object_1= SampleClass(0)
+object_2=SampleClass(2)
+object_3= object_1
+object_3.val +=1
+
+print(object_1 is object_2)
+print(object_2 is object_3)
+print(object_3 is object_1)
+
+print(object_1.val,object_2.val,object_3.val)
+
+string_1="Mary had a little "
+string_2="Mary had a little lamb"
+string_1+="lamb"
+print(string_1==string_2,string_1 is string_2)
+
--- 21_classvariable.py
+++ 21_classvariable.py
@@ -1,13 +1,88 @@
-class ExampleClass:
-    counter = 0
-    def __init__(self,val=1):
-        self.__first=val
-        ExampleClass.counter +=1
+# class ExampleClass:
+#     counter = 0
+#     def __init__(self,val=1):
+#         self.__first=val
+#         ExampleClass.counter +=1
 
-example_object_1= ExampleClass()
-example_object_2=ExampleClass(2)
-example_object_3=ExampleClass(4)
+# example_object_1= ExampleClass()
+# example_object_2=ExampleClass(2)
+# example_object_3=ExampleClass(4)
 
-print(example_object_1.__dict__,example_object_1.counter)
-print(example_object_2.__dict__,example_object_2.counter)
-print(example_object_3.__dict__,example_object_3.counter)
+# print(example_object_1.__dict__,example_object_1.counter)
+# print(example_object_2.__dict__,example_object_2.counter)
+# print(example_object_3.__dict__,example_object_3.counter)
+
+# print(ExampleClass.counter)
+
+# class ExampleClass:
+#     a=1
+#     counter = 0
+#     def __init__(self,val=1):
+#         ExampleClass.counter +=1
+#         if val%2!=0:
+#             self.a=1
+#         else:
+#             self.b=1
+# example_object= ExampleClass(1)
+# # way 1 with try catch
+# try:
+#     print("a =",example_object.a)
+# except AttributeError:
+#     print("a is not defined")
+# try:
+#     print("b =",example_object.b)
+# except AttributeError:
+#     print("b is not defined")
+
+# way 2 with try catch nested try catch
+# try:
+#     print("a =",example_object.a)
+# except AttributeError:
+#     print("a is not defined")
+#     try:
+#         print("b =",example_object.b)
+#     except AttributeError:
+#         print("b is not defined")
+
+# way 3 with hasattr
+# if hasattr(example_object,'a'):
+#     print("a =",example_object.a)
+# if hasattr(example_object,'b'):
+#     print("b =",example_object.b)
+
+# print (hasattr(ExampleClass,'a'))
+# print (hasattr(ExampleClass,'b'))
+
+
+# class Python:
+#     population=1
+#     victims=0
+#     def __init__ (self):
+#         self . length_ft=3
+#         self.__venemous =False
+# myObj =Python()
+# print("myObj.population: ",myObj.population)
+# print("myObj.victims :",myObj.victims)
+# print("myObj.length_ft :",myObj.length_ft)
+# print("myObj.__venemous :",myObj._Python__venemous)
+
+
+#name mangling
+class Classy:
+    def visible(self):
+        print("visible")
+    def __hidden(self):
+        print("hidden")
+        
+obj= Classy()
+obj.visible()
+try:
+    obj.__hidden()
+except:
+    print("failed")
+obj._Classy__hidden()
+
+
+obj =Classy()
+print(type(obj))
+print(type(obj).__name__)
