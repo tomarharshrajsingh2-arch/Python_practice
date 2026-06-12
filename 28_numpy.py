79bff6bb54401d5199d5a81580d973e285aed144
Harshraj Singh Tomar <tomarharshrajsingh2@gmail.com>
2026-06-12 17:11:25 +0530

    numpy

---
+++ 28_numpy.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+# arr1d = np.array([1, 2, 3, 4, 5])
+# print(arr1d)
+# print(arr1d.shape)
+# print(arr1d.ndim)
+# print(arr1d.dtype)
+# print("------------------")
+# arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr2d)
+# print(arr2d.shape)
+# print(arr2d.ndim) 
+# print(arr2d.dtype)
+
+
+
+
+# Zeros = np.zeros((3, 4))
+# print(Zeros)
+# Ones = np.ones((2, 5))
+# print(Ones)
+# rng = np.arange(0, 50, 5)
+# print(rng)
+
+# lin = np.linspace(0, 1, 11) 
+# print(lin)
+
+# random=np.random.randint(40,100,(5,3))
+# print(random)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr * 2)
+# print(arr + 10)
+# print(arr ** 2)
+
+
+# marks_2d = np.array([[85, 90, 78], [92, 88, 95], [80, 85, 82]])
+# print(np.mean(marks_2d))
+# print(np.mean(marks_2d, axis=1))
+# print(np.mean(marks_2d, axis=0))
+# print(np.max(marks_2d))
+# print(np.min(marks_2d))
+# print(np.std(marks_2d))
+
+
+arr= np.array([55,62,43,78,90])
+print(arr[arr > 70])
